20224bfabe3c78707e35fd81661bad321673825e
Jason Carmichael <jasonc0308@icloud.com>
2022-11-03 13:29:35 -0500

    api data rendering on screen

--- current-bestsellers/src/components/Books.jsx
+++ current-bestsellers/src/components/Books.jsx
@@ -1,17 +1,17 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import { BASE_URL } from '../globals'
 
 export default function Books () {
 
-    // we need to create a state for our data
-    const [books, setBooks] = useState({})
+    const [books, setBooks] = useState([])
 
 
     useEffect(()=> {
         const getData = async () => {
-            const response = await axios.get('https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=J7nIpJdXUz0qOYrBVbZGKKBzQAN0Y7dQ')
-            console.log(response.data)
-            setBooks(response.data)
+            const response = await axios.get(`${BASE_URL}/lists/current/hardcover-fiction.json?api-key=${process.env.REACT_APP_NYT_KEY}`)
+            console.log(response.data.results.books)
+            setBooks(response.data.results.books)
         }
 
         getData()
@@ -21,23 +21,15 @@ export default function Books () {
     console.log(books)
 
 
-
-    // wee need to set the state of our data
-
-    /// wee need to see our data
-
-    // we need to make a guard operator
-    // so if data takes a few seconds
-    // our site doesnt break
-
     if (!books) {
         return <h2> Loading please wait </h2>
     } else {
         return (
-            <div>
-                <h3>Title: {books.title}</h3>
-                <h4>Author: {books.author}</h4>
-            </div>
+            books.map((book) => (
+                <div key={book.isbn}>
+                    <h3>Title: {book.title}</h3>
+                    <h4>Author: {book.author}</h4>
+                </div> ))
         )
     }
 }
