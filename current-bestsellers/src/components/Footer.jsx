61e783292d26d8ae1955f4ca06501c1b9c8c3438
Jason Carmichael <jasonc0308@icloud.com>
2022-11-03 10:44:55 -0500

    set up basic components and cleanup

---
+++ current-bestsellers/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer () {
+
+    return (
+        <div>
+            <h2> 2022 JDC </h2>
+        </div>
+    )
+}
---
+++ current-bestsellers/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import Nav from './Nav'
+
+export default function Header () {
+
+    return (
+        <div>
+            <h2> Current Bestsellers </h2>
+            <Nav />
+        </div>
+    )
+}
