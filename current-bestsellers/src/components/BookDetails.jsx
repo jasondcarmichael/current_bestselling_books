6e3fa854c78e13268f3ee4d3bcd7e1a89aa93668
Jason Carmichael <jasonc0308@icloud.com>
2022-11-03 20:25:51 -0500

    pseudocode book details component

--- current-bestsellers/src/components/BookDetails.jsx
+++ current-bestsellers/src/components/BookDetails.jsx
@@ -1,4 +1,24 @@
 export default function BookDetails () {
+
+    // see Boats Lesson
+    
+    // import data from Books component (props)
+
+    // pass down data for the specified Book id(isbn#)
+
+    // this component loads onClick() button from Book component
+
+    // identify all book details to pull
+
+    // make elements for all details
+
+    // Render all data
+
+    // Close window or back button
+
+    // Needs unique url ex. hardcover-fiction/isbn
+    
+    
     return (
         
     )
